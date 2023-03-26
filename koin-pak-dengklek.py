5d7988ba4b247eb870c5ae9424bcdd0d83619964
muhammadfikri <50939234+ClaysHere@users.noreply.github.com>
2023-03-26 14:39:11 +0700

    Create koin-pak-dengklek.py

---
+++ koin-pak-dengklek.py
@@ -0,0 +1,35 @@
+def coin(s,m):
+    for i in range(len(s)):
+        if i == 0:
+            if s[i] != string[(i+1)] and s[i] != s[i+2]:
+                m = i + 1 
+        elif i == (len(string)-1):
+            if s[i] != s[(i-1)] and s[i] != s[i-2]:
+                m = i + 1
+        else:
+            if s[i] != s[(i-1)] and s[i] != s[i+1]:
+                m = i + 1
+    
+    print(m)
+
+s = input()
+
+string = []
+for i in range(len(s)):
+    string.append(s[i])
+
+maxi = -1
+
+n = 0
+
+for i in range(len(string)):
+    if i > (len(string)-1):
+        if string[i] == string[i+1]:
+            n+=1
+
+if n == 8:
+    print(maxi)
+else:
+    coin(string,maxi)
+    
+# kompleksitas = O(n)
